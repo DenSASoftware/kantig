80a6bb1a4e4dc3ab2cb28a3409f5aa29c2b0e0b9
DenSASoftware <social@densa.software>
2020-08-31 01:20:50 +0200

    add documentation to the cli parameters

--- src/opts.rs
+++ src/opts.rs
@@ -66,40 +66,60 @@ fn parse_image_format(src: &str) -> Result<ImageFormat, ImageFormatError> {
 }
 
 #[derive(Debug, StructOpt)]
+#[structopt(name = "low-poly")]
+/// Create low-poly images
+///
+/// Transform an image into a low-poly image. This program applies an edge-detection-algorithm,
+/// selects a few of the resulting points and uses the delaunay-algorithm to create a mesh of
+/// triangles that will be turned into the final image.
 pub struct Options {
+    /// the lower bound for the edge detectioin algorithm
     #[structopt(long, parse(try_from_str = parse_positive_float), default_value = "10.0")]
     pub canny_lower: f32,
 
+    /// the upper bound for the edge detection algorithm
     #[structopt(long, parse(try_from_str = parse_positive_float), default_value = "15.0")]
     pub canny_upper: f32,
 
+    /// the number of points to be picked from the edges
     #[structopt(short, long)]
     pub points: Option<usize>,
 
+    /// pick an amount of low-poly points relative to the number of edge-points, 0 means none and 1
+    /// means all
     #[structopt(long, parse(try_from_str = parse_float_between_one_zero))]
     pub points_relative: Option<f32>,
 
+    /// pick an amount of low-poly points relative to the number of pixels in the image, 0 means
+    /// none and 1 means all
     #[structopt(long, parse(try_from_str = parse_float_between_one_zero))]
     pub points_pixel_relative: Option<f32>,
 
+    /// enforce a minimum distance between low-poly points
     #[structopt(long, parse(try_from_str = parse_positive_float), default_value = "4")]
     pub points_min_distance: f32,
 
+    /// a shell command to map color values in the final image
     #[structopt(long)]
     pub color_mapper: Option<String>,
 
+    /// do not use antialiasing when drawing polygons
     #[structopt(long)]
     pub no_antialiasing: bool,
 
+    /// the seed for the random number generator, values from 0 to 2^16-1 are acceptable
     #[structopt(long)]
     pub rng_seed: Option<u128>,
 
+    /// the output file name, defaults to stdout
     #[structopt(long, short, parse(from_os_str))]
     pub output: Option<PathBuf>,
 
+    /// the output format, overrides the file format detected in the output file name
     #[structopt(long, parse(try_from_str = parse_image_format))]
     pub output_format: Option<ImageFormat>,
 
+    /// the input file name, defaults to stdin
     #[structopt(parse(from_os_str))]
     pub input: Option<PathBuf>,
 }
